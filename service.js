7026e58a7312b8236da5bf226d3c1f2b5fd988c5
fnncat <fnncat@fnncatdeMacBook-Pro.local>
2017-11-04 19:54:45 +0800

    into db

--- service.js
+++ service.js
@@ -5,7 +5,13 @@ var request = require('request');
 var i = 0;
 var page_c=16;//页数
 var url = "http://www.cnblogs.com/fnncat";//blog网址
-
+var mongodb= 'mongodb://localhost:27017/myblog';
+var mongodb = require('mongodb');
+var Mongolass = require('mongolass');
+var mongolass = new Mongolass();
+mongolass.connect(mongodb);
+var server = new mongodb.Server('localhost',27017,{auto_reconnect:true});
+var db = new mongodb.Db('myblog',server,{safe:true});
 function fetchPage(x) {
     console.log("into the fetchPage")
     saveAllUrl(page_c);   
@@ -14,6 +20,7 @@ function saveAllUrl(page_c){
 var url_list_co=[];
 var urls_list=[];
 var co=0;
+db.open(function(err,db){     });
  for(var j=1;j<page_c+1;j++){ 
     console.log(j)
     var title_url=url+"/default.html?page="+j;
@@ -50,21 +57,35 @@ var co=0;
 
 function startRequest(x,co) {
     console.log("into the startRequest"+co)    
-    http.get(x, function (res) {     
+    http.get(x, function (res) { 
         var html = '';          
         res.setEncoding('utf-8');
+        res.on('data', function (chunk) {   
+            html += chunk;
+        });
         res.on('end', function () {
+              console.log(cheerio); 
          var $ = cheerio.load(html); 
          var news_item = {
+            author:Mongolass.Types.ObjectId("59f2aaef93c6ca0488384505"),
             title: $('#cb_post_title_url').text().trim(),
-            Time:  $('#post-date').text().trim(),
-            link: x,
-            content:$('#cnblogs_post_body')   
+            creat_time: $('#post-date').text().trim(),
+            label:"老站内容",
+            url:x,
+            content:$('#cnblogs_post_body').text().trim(),
+            pv:0
             };
       var news_title = $('.postTitle a').text().trim();
-      console.log(news_item);
+
       savedContent($,news_title,news_item,co);  //存储每篇文章的内容及文章标题
-     });
+            db.collection('posts',{safe:true},function(err,collection){
+                collection.insert(news_item,{safe:true},function(err,request){
+                    console.log(news_item)
+                })
+            })
+        
+      })
+
 
     }).on('error', function (err) {
         console.log(err);
@@ -79,6 +100,9 @@ function savedContent($, news_title,news_item,co) {
                 console.log(err);
             }
         });   
+}
+function saveDb(){
+
 }
 
 fetchPage(url);
